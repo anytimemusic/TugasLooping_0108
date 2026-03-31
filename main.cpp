089e90b5b5770ddce30cea5c71646a1983f61a5e
anytimemusic <bima220711@gmail.com>
2026-03-31 19:27:31 +0700

    func cek bil fibo

--- main.cpp
+++ main.cpp
@@ -16,4 +16,19 @@ bool cekPrima(int x) {
         i++;
     }
     return true;
-} 
+}
+
+// Function cek bilangan Fibonacci
+bool cekFibonacci(int x) {
+    int a = 0, b = 1, c;
+
+    while (a <= x) {
+        if (a == x) {
+            return true;
+        }
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return false;
+}
