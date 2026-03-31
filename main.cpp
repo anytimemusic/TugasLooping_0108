e7bea0ab122968b8edaf607f973d8f5ed71ec2e9
anytimemusic <bima220711@gmail.com>
2026-03-31 19:26:34 +0700

    func cek bil prima

--- main.cpp
+++ main.cpp
@@ -4,3 +4,16 @@ using namespace std;
 // Variabel global
 int n, pilihan;
 
+// Function cek bilangan prima
+bool cekPrima(int x) {
+    if (x <= 1) return false;
+
+    int i = 2;
+    while (i <= x / 2) {
+        if (x % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
